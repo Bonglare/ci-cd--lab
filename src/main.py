4f8de6bd690d55d320e6820e09c4baa8413754ae
Bongalre <rjho121@hotmail.com>
2026-05-17 16:44:25 +0000

    Fix indentation error in main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+
 def load_data():
-data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
-return pd.DataFrame(data)
+    # Example DataFrame
+    data = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+    return pd.DataFrame(data)
